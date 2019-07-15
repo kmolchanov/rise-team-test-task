d4e71e2fbccfe61eb612631b1e1bc221528b8726
Konstantin Molchanov <kostya.molchanov@gmail.com>
2019-07-15 18:00:14 +0600

    Fixes

--- console/config/main.php
+++ console/config/main.php
@@ -1,10 +1,4 @@
 <?php
-$params = array_merge(
-    require __DIR__ . '/../../common/config/params.php',
-    require __DIR__ . '/../../common/config/params-local.php',
-    require __DIR__ . '/params.php',
-    require __DIR__ . '/params-local.php'
-);
 
 return [
     'id' => 'app-console',
@@ -32,5 +26,4 @@ return [
             ],
         ],
     ],
-    'params' => $params,
 ];
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -1,10 +1,4 @@
 <?php
-$params = array_merge(
-    require __DIR__ . '/../../common/config/params.php',
-    require __DIR__ . '/../../common/config/params-local.php',
-    require __DIR__ . '/params.php',
-    require __DIR__ . '/params-local.php'
-);
 
 return [
     'id' => 'app-frontend',
@@ -44,5 +38,4 @@ return [
             ],
         ],
     ],
-    'params' => $params,
 ];
